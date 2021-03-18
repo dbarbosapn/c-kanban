4a49bb54f093a1268b42e18359eddc1857254eb9
Diogo Barbosa <pessoal.dbarbosa@gmail.com>
2021-03-18 15:13:07 +0000

    Finished linked-lists

--- lib/linked_list.h
+++ lib/linked_list.h
@@ -12,9 +12,15 @@ Node *list_prepend(Node *head, void *value, size_t alloc_size);
 
 Node *list_append(Node *head, void *value, size_t alloc_size);
 
+Node *list_add_inorder(Node *head, void *value, size_t alloc_size,
+                       int (*comparator)(void *, void *));
+
 Node *list_get(Node *head, int index);
 
 Node *list_remove(Node *head, Node *to_remove);
 
-void list_serialize(FILE *output, Node *head,
-                    void (*serializer)(void *, FILE *));
+Node *list_sort(Node *head, int (*comparator)(void *, void *));
+
+char *list_serialize(Node *head, char *(*serializer)(void *));
+
+Node *list_deserialize(char *input, void *(*deserializer)(char *));
