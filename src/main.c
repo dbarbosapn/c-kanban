eb539614f2de566e71469dd825a0ba23c4ee1c14
Diogo Barbosa <pessoal.dbarbosa@gmail.com>
2021-03-21 19:48:09 +0000

    Implements saving and loading

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
+#include <kanban_task.h>
 #include <linked_list.h>
 #include <stdio.h>
 #include <unistd.h>
 
 #define TASKS_FILE "data/all_tasks.data"
 
+/**
+ * Checks if a file exists
+ **/
 int file_exists(char* path) { return access(path, F_OK) == 0; }
 
 /**
@@ -18,14 +22,26 @@ Node* load_data() {
         if (!fp || feof(fp) || !fgets(buffer, sizeof(buffer), fp)) {
             printf("There was an error loading the data file. (%s)\n",
                    TASKS_FILE);
+            fclose(fp);
             return NULL;
         }
 
-        // TODO: Use the buffer to deserialize the list. Pass the task
-        // deserializer too.
+        fclose(fp);
+        return list_deserialize(buffer, TASK_SERIALIZE_BUFFER_SIZE,
+                                task_deserialize);
     }
 
     return NULL;
+}
+
+/**
+ * Saves the given list in the data file
+ **/
+void save_data(Node* all_tasks) {
+    char* str = task_deserialize(all_tasks);
+    FILE* fp = fopen(TASKS_FILE, "w");
+    fputs(str, fp);
+    fclose(fp);
 }
 
 int main(int argc, char const* argv[]) {
@@ -33,6 +49,8 @@ int main(int argc, char const* argv[]) {
     printf("Loading data...\n");
     Node* all_tasks = load_data();
     printf("Loaded!\n");
+
+    // TODO: Graphics routine. Always show data on top, commands on bottom.
 
     printf("This is the main file.\n");
     return 0;
