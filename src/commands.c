80fb7a42457e5b1f1b5042aa3708d0d3fa43395b
Diogo <diogoaluai@gmail.com>
2021-03-26 20:49:55 +0000

    Adds task search function

--- src/commands.c
+++ src/commands.c
@@ -1,14 +1,16 @@
 #include <commands.h>
 
-void command_insert_new_task(Node *todo, int id, char *desc, int priority) {
+int command_insert_new_task(Node *todo, int id, char *desc, int priority) {
      KanbanTask *task = create_task(id,desc,priority);
 
      //    needs comparator
      list_add_inorder(todo, task, sizeof(Node), todo_comparator);
+     return 0;
 }
 
-void command_move_to_doing(Node* todo, Node* doing, lond task_id, char *worker, time_t deadline) {
-     Node *node = list_get_with_id(todo, task_id);
+int command_move_to_doing(Node* todo, Node* doing, lond task_id, char *worker, time_t deadline) {
+     Node *node = get_task_using_id(todo, task_id);
+     if(node == NULL) return -1;
      KanbanTask *task = node->value;
 
      task->worker = malloc(strlen(worker)+1);
@@ -18,11 +20,13 @@ void command_move_to_doing(Node* todo, Node* doing, lond task_id, char *worker, 
      task->deadline = deadline;
      list_add_inorder(doing, task, sizeof(Node), doing_comparator);
      list_remove(todo, node);
+     return 0;
 }
 
 
-void command_move_to_todo(Node* todo, Node* doing, long task_id) {
-     Node *node = list_get_with_id(doing, task_id);
+int command_move_to_todo(Node* todo, Node* doing, long task_id) {
+     Node *node = get_task_using_id(doing, task_id);
+     if(node == NULL) return -1;
      KanbanTask *task = node->value;
 
      free(task->worker);
@@ -31,19 +35,23 @@ void command_move_to_todo(Node* todo, Node* doing, long task_id) {
 
      list_add_inorder(todo, task, sizeof(Node), doing_comparator);
      list_remove(doing, node);
+     return 0;
 }
 
-void command_change_responsable(Node *doing, long task_id, char *worker) {
-     Node *node = list_get_with_id(doing, task_id);
+int command_change_responsable(Node *doing, long task_id, char *worker) {
+     Node *node = get_task_using_id(doing, task_id);
+     if(node == NULL) return -1;
      KanbanTask *task = node->value;
 
      free(task->worker);
      task->worker = malloc(strlen(worker)+1);
      strcpy(task->worker, worker);
+     return 0;
 }
 
-void command_end_task(Node *doing,Node *done, long task_id) {
-     Node *node = list_get_with_id(doing, task_id);
+int command_end_task(Node *doing,Node *done, long task_id) {
+     Node *node = get_task_using_id(doing, task_id);
+     if(node == NULL) return -1;
      KanbanTask *task = node->value;
 
      time_t curr_time;
@@ -53,10 +61,12 @@ void command_end_task(Node *doing,Node *done, long task_id) {
 
      list_add_inorder(done, task, sizeof(Node), done_comparator);
      list_remove(doing, node);
+     return 0;
 }
 
-void command_reopen(Node *todo,Node *done, long task_id) {
-     Node *node = list_get_with_id(doing, task_id);
+int command_reopen(Node *todo,Node *done, long task_id) {
+     Node *node = get_task_using_id(doing, task_id);
+     if(node == NULL) return -1;
      KanbanTask *task = node->value;
 
      free(task->worker);
@@ -66,4 +76,16 @@ void command_reopen(Node *todo,Node *done, long task_id) {
 
      list_add_inorder(todo, task, sizeof(Node), todo_comparator);
      list_remove(done, node);
+     return 0;
+}
+
+
+Node *get_task_using_id(Node *head, long task_id) {
+     Node *curr = head;
+     while (curr != NULL && curr->id != task_id) {
+          curr = curr->next;
+     }
+     if (curr == NULL) return NULL;
+
+     return curr;
 }
--- src/commands.h
+++ src/commands.h
@@ -2,14 +2,14 @@
 #include <kanban_task.c>
 
 
-void command_insert_new_task(Node *todo, int id, char *desc, int priority);
+int command_insert_new_task(Node *todo, int id, char *desc, int priority);
 
-void command_move_to_doing(Node* todo, Node* doing, lond task_id, char *worker, time_t deadline);
+int command_move_to_doing(Node* todo, Node* doing, lond task_id, char *worker, time_t deadline);
 
-void command_move_to_todo(Node* todo, Node* doing, long task_id);
+int command_move_to_todo(Node* todo, Node* doing, long task_id);
 
-void command_change_responsable(Node *doing, long task_id, char *worker);
+int command_change_responsable(Node *doing, long task_id, char *worker);
 
-void command_end_task(Node *doing,Node *done, long task_id);
+int command_end_task(Node *doing,Node *done, long task_id);
 
-void command_reopen(Node *todo,Node *done, long task_id);
+int command_reopen(Node *todo,Node *done, long task_id);
