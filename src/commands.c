5a2429cf2806a5f0e3274eed458494b6faaedecf
Diogo <diogoaluai@gmail.com>
2021-03-26 20:33:59 +0000

    Adds states and better identation

--- src/commands.c
+++ src/commands.c
@@ -1,6 +1,4 @@
-// #include <commands.h>
-#include <linked_list.c>
-#include <kanban_task.c>
+#include <commands.h>
 
 void command_insert_new_task(Node *todo, int id, char *desc, int priority) {
      KanbanTask *task = create_task(id,desc,priority);
@@ -15,6 +13,7 @@ void command_move_to_doing(Node* todo, Node* doing, lond task_id, char *worker, 
 
      task->worker = malloc(strlen(worker)+1);
      strcpy(task->worker, worker);
+     task->state = DOING;
 
      task->deadline = deadline;
      list_add_inorder(doing, task, sizeof(Node), doing_comparator);
@@ -25,8 +24,11 @@ void command_move_to_doing(Node* todo, Node* doing, lond task_id, char *worker, 
 void command_move_to_todo(Node* todo, Node* doing, long task_id) {
      Node *node = list_get_with_id(doing, task_id);
      KanbanTask *task = node->value;
+
      free(task->worker);
      task->deadline = NULL;
+     task->state = TODO;
+
      list_add_inorder(todo, task, sizeof(Node), doing_comparator);
      list_remove(doing, node);
 }
@@ -34,6 +36,7 @@ void command_move_to_todo(Node* todo, Node* doing, long task_id) {
 void command_change_responsable(Node *doing, long task_id, char *worker) {
      Node *node = list_get_with_id(doing, task_id);
      KanbanTask *task = node->value;
+
      free(task->worker);
      task->worker = malloc(strlen(worker)+1);
      strcpy(task->worker, worker);
@@ -46,6 +49,7 @@ void command_end_task(Node *doing,Node *done, long task_id) {
      time_t curr_time;
      time(&curr_time);
      task->finish_date = curr_time;
+     task->state = DONE;
 
      list_add_inorder(done, task, sizeof(Node), done_comparator);
      list_remove(doing, node);
@@ -58,6 +62,7 @@ void command_reopen(Node *todo,Node *done, long task_id) {
      free(task->worker);
      task->deadline = NULL;
      task->finish_date = NULL;
+     task->state = TODO;
 
      list_add_inorder(todo, task, sizeof(Node), todo_comparator);
      list_remove(done, node);
